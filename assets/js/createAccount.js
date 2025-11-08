4835b3f469e086e4dd50ef95f876aad481796104
Rangel-tt7 <artrangel07@gmail.com>
2025-11-07 21:15:37 -0300

    Funcionamento e validação da página Login

--- assets/js/createAccount.js
+++
@@ -1,9 +0,0 @@
-if (document.getElementById("name").value == ''){
-    let aviso = document.createElement("p");
-    aviso.className = "erros";
-    const div = document.getElementById("semnome");
-    div.appendChild(aviso);
-    aviso.textContent = "Por favor preencha seu nome."
-} else {
-    aviso.remove();
-}
