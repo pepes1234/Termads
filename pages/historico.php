7923387e96ec3540d6196081f636be5a068dc21e
Rodrigo Yuji <yuji.rodrigo@ufpr.br>
2025-11-10 17:21:12 -0300

    Navbar

--- pages/historico.php
+++ pages/historico.php
@@ -10,9 +10,23 @@
     <title>Histórico - Termads</title>
 </head>
 <body>
+    <nav>
+        <ul>
+            <li><a href="game.php">Jogar</a></li>
+            <li><a href="#">Ligas</a></li>
+            <li><a href="#">Classificação</a></li>
+            <li><a href="historico.php">Histórico</a></li>
+        </ul>
+        <ul>
+            <li><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+            </svg>Perfil</a></li>
+        </ul>
+    </nav>
     <div class="main-container">
         <div class="header">
-            <a href="../index.html" class="back-button">← Voltar</a>
+            <a href="game.php" class="back-button">← Voltar</a>
             <h1 class="page-title">Histórico de Partidas</h1>
         </div>
 
