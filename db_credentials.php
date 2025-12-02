f17f011a8cf8e62cc44b831a0819cd7814893073
Rodrigo Yuji <yuji.rodrigo@ufpr.br>
2025-12-02 15:02:14 -0300

    Alterações - Log, NavBars

--- db_credentials.php
+++ db_credentials.php
@@ -2,6 +2,6 @@
 /* Database credentials */
 $servername = "localhost";
 $username = "root";
-$password = getenv('DB_PASSWORD') ?? "";
+$password = "hamburguer1203";
 $dbname = "termads_db";
 $table_users = "users";
